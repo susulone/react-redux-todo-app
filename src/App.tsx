e4a869f56b3fd821f8a44fb3cd82bbcf0d87821b
Suvi Sulonen <suvi.sulonen@gmail.com>
2023-12-09 22:24:51 +0200

    Add Header, TaskList and Footer components into App. Edit App.css and index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+// Components
+import { Container } from "react-bootstrap";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { TaskList } from "./components/TaskList/TaskList";
+
+// Interfaces, types and styles
 import "./App.css";
 
-export interface TodoInterface {
+export interface TaskInterface {
     id: string | number;
     task: string;
     completed: boolean;
 }
 
 function App() {
-    return <main></main>;
+    return (
+        <Container fluid>
+            <Header />
+            <TaskList />
+            <Footer />
+        </Container>
+    );
 }
 
 export default App;
